98910453a3d8dceb84a7610df923f5b5a9ecd066
Ioannis Tsampras <gtsambras@gmail.com>
2024-01-14 22:45:22 +0200

    clustering done

--- tasks/Part_B/clustering.py
+++ tasks/Part_B/clustering.py
@@ -31,7 +31,7 @@ def load_to_memory(original_file_path):
 original_file_path_merged = 'tasks/Part_B/merged.csv'
 dataset_merged=load_to_memory(original_file_path=original_file_path_merged)
 
-# we will remove the frail column (index = 9) as it is the obtained label. the project description does not elaborate on the 5 columns that produce the frail index
+# we will remove the frail column (index = 9) as it is the obtained label. the project description does not elaborate on the 5 columns that produce the frail index. 
 temp=[]
 for i in dataset_merged:
     temp.append(i[0:9]+i[9+1:])
@@ -40,9 +40,63 @@ dataset_merged=temp
 
 header=dataset_merged[0]
 entries=dataset_merged[1:]
-print(header)
+#print(header)
 
 #we now cast to float
 entries=cast_to_float_2d(entries)
-print(entries[0])
+
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+import numpy as np
+
+# Assuming your data is stored in a 2D list named 'data'
+# Each row corresponds to a data point, and columns are features
+# Ignore the first column (row id)
+
+
+# Convert the 2D list to a numpy array and exclude the first column since it is a row id 
+data = np.array([row[1:] for row in entries])
+
+mean = np.mean(data, axis=0)
+
+# Normalize the dataset
+normalized_data = (data)/mean  #this way we equalie the weight of each field to the output
+X=normalized_data
+
+error_list=[]
+sil_list=[]
+for i in range(2,20):
+    # Define the number of clusters (k)
+    k = i
+
+    # Apply k-means clustering
+    kmeans = KMeans(n_clusters=i, random_state=42,n_init=20,max_iter=500)
+    labels = kmeans.fit_predict(X)
+    wcss = kmeans.inertia_
+    error_list.append(wcss/10000)
+
+    # Calculate silhouette score
+    silhouette_avg = silhouette_score(X, labels)
+    sil_list.append(silhouette_avg)
+    #print(f"Silhouette Score for {i} clusters: {silhouette_avg}")
+
+import matplotlib.pyplot as plt
+
+# Create a plot with the first list
+plt.plot(error_list, label='WCSS/10000')
+
+# Add the second list to the same plot
+plt.plot(sil_list, label='Silhouette Index')
+
+# Add labels and a legend
+plt.title('Plot of Two Lists')
+plt.xlabel('X-axis Cluster Number')
+plt.ylabel('Y-axis (Values)')
+plt.legend()
+
+# Show the plot
+plt.show()
+
+#from the plot we can visually identify the elbow on n=9 clusters and the corresponding silhoutte index is 0.291
 
